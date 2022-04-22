a03d9bd9020322a076b0583f690406730abcd699
Gnosis <emmagab38@gmail.com>
2022-04-22 01:10:33 +0100

    frontend

--- resources/views/dashboard2/layouts/app.blade.php
+++ resources/views/dashboard2/layouts/app.blade.php
@@ -237,6 +237,20 @@
     <script src="{{ asset('front/asset/dashboard/js/demo.min.js') }}"></script>
     <script src="{{ asset('front/asset/frontend/js/sweetalert.js') }}"></script>
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/625151957b967b1179898e70/1g06qdcle';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
 </body>
 
 </html>
